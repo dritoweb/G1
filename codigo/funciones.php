67e7a79d8e096399ff711a02cc711d754575df20
Javier Castro <javier.castro.secilla@ieslaarboleda.es>
2021-05-04 13:41:18 +0200

    estructuras2

--- codigo/funciones.php
+++ codigo/funciones.php
@@ -5,8 +5,12 @@ function CrearEstructura(){
 	mysqli_query ($c, "CREATE DATABASE IF NOT EXISTS $base");
 	mysqli_select_db($c,"$base");
 	$crear="CREATE TABLE IF NOT EXISTS animadores(idAnimador CHAR(3), NombreAnimador VARCHAR(20), Especialidad VARCHAR(20), Precio FLOAT(5,2),
-		PRIMARY KEY (idAnimador))";
+	       PRIMARY KEY (idAnimador))";
     $crear2="CREATE TABLE IF NOT EXISTS Clientes(idCliente CHAR(3), NombreCliente VARCHAR(20), Direccion VARCHAR(20),Email VARCHAR(20), PRIMARY KEY(idCliente))";
-    $crear3="CREATE TABLE IF NOT EXISTS Fiestas(idFiesta CHAR(3), Fecha DATE, Especialidad VARCHAR(20), Duraccion INT(3), TipodeFiesta VARCHAR(15), Numero , EdadMedia INT(2), Importe FLOAT(5,2), IdCliente, PRIMARY KEY (idFiesta), F),)";
+    $crear3="CREATE TABLE IF NOT EXISTS Fiestas(idFiesta CHAR(3), Fecha DATE, Especialidad VARCHAR(20), Duraccion INT(3), TipodeFiesta VARCHAR(15), Numero INT(3), 
+            EdadMedia INT(2), Importe FLOAT(5,2), IdCliente CHAR(3), PRIMARY KEY (idFiesta),FOREING KEY (Especialidad) REFERENCES animadiores(Especialidad)
+            ,FOREING KEY (idCliente) REFERENCES Clientes(idCliente), ON DELETE CASCADE)";
+    $crear4="CREATE TABLE IF NOT EXISTS Tipodefiestas(idFiesta CHAR(3), NombreFiesta VARCHAR(20), PRIMARTY KEY (idFiesta),FOREING KEY (idFiesta) REFERENCES Fiestas(idFiestas))";
 }
+
 ?>
